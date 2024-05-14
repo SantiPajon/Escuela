8be10f67c863603650ac58244cad191410a412b4
Santiago Pajon <Pajons23@gmail.com>
2024-05-13 21:04:58 -0300

    Primer comit: Generamos Apps y modelos

---
+++ Clase/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self) -> str:   
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+    
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    nacimiento = models.DateField(null=True, blank=True)
+    
+    
+    def __str__(self) -> str:
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Estudiante"
+        verbose_name_plural = "Estudiantes"
+        
+        
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Profesor"
+        verbose_name_plural = "Profesores"
