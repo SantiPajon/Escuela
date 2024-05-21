9856f5d598a61a4bc1f7e6354f3a4835f3c1840e
Santiago Pajon <Pajons23@gmail.com>
2024-05-21 17:57:18 -0300

    feat: login - logout - register

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from core.views import register
+from core.views import CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/",CustomLoginView.as_view(),name="login"),
+    path("logout/",LogoutView.as_view(template_name="core/logout.html"),name="logout"),
+    path("register/",register, name="register")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from core.forms import CustomAuthenticationForm,CustomUserCreationForm
+from django.contrib.auth.views import LoginView 
 
 # Create your views here.
 
 def index(request):
     return render(request,"core/index.html")
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
+    
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            form.save()
+            return render(request,"core/index.html",{"mensaje":f"Usuario {username} creado"})
+        
+    else:
+        form = CustomUserCreationForm()
+    return render(request,"core/register.html",{"form":form})
